b10ad30c5849d487ced6c98a992e733701cd8424
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-01-12 00:50:06 -0500

    Create FooTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Feature/FooTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Feature;
+
+use App\Foo;
+use App\Tests\AbstractTestCase;
+
+/** @coversDefaultClass \App\Foo */
+class FooTest extends AbstractTestCase
+{
+    /** @covers ::test */
+    public function test(): void
+    {
+        $this->assertTrue((new Foo())->test());
+    }
+}

4483b212c77b0dcfe9a70974bad6d3d9e9340f24
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-18 23:26:26 -0400

    Create NoneInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Contract/NoneInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Option\Contract;
+
+/**
+ * @implements OptionInterface<TValue>
+ */
+interface NoneInterface extends OptionInterface
+{
+}

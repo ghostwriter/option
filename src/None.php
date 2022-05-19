f9e7e734509e2f5f6235921d822d9d5301b7801b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-18 23:28:27 -0400

    Create None.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/None.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Option;
+
+use Ghostwriter\Option\Contract\NoneInterface;
+
+/**
+ * @extends AbstractOption<TValue>
+ * @immutable
+ * @implements NoneInterface<TValue>
+ * @template TValue
+ */
+final class None extends AbstractOption implements NoneInterface
+{
+    private static ?NoneInterface $instance = null;
+
+    private function __construct()
+    {
+        // Singleton
+    }
+
+    public static function create(): NoneInterface
+    {
+        return self::$instance ??= new self();
+    }
+}

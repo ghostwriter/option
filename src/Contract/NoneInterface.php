d1d6470d0ca43420af2b06484f2240b89a370493
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-18 23:27:20 -0400

    Update NoneInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/NoneInterface.php
+++ src/Contract/NoneInterface.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace Ghostwriter\Option\Contract;
 
 /**
+ * @immutable
  * @implements OptionInterface<TValue>
+ * @template TValue
  */
 interface NoneInterface extends OptionInterface
 {
+    /**
+     * @return self<TValue>
+     */
+    public static function create(): self;
 }

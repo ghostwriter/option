815a9fe8ead36fea51545aec3bbfae2c97ad12f2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-18 23:27:04 -0400

    Create SomeInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Contract/SomeInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Option\Contract;
+
+/**
+ * @immutable
+ * @implements OptionInterface<TValue>
+ * @template TValue
+ */
+interface SomeInterface extends OptionInterface
+{
+}

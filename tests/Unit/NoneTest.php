1c510c4dfb050e37f023a1a53bbe3fa48199b560
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-08-03 18:34:43 -0500

    Update NoneTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/NoneTest.php
+++ tests/Unit/NoneTest.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace Ghostwriter\Option\Tests\Unit;
 
 use Ghostwriter\Option\Contract\NoneInterface;
-use Ghostwriter\Option\Contract\OptionInterface;
-use Ghostwriter\Option\Exception\InvalidReturnTypeException;
 use Ghostwriter\Option\Exception\NullPointerException;
 use Ghostwriter\Option\None;
 use Ghostwriter\Option\Some;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
-use stdClass;
 use Throwable;
 
 /**
@@ -56,7 +53,7 @@ final class NoneTest extends TestCase
      */
     public function testAndThen(): void
     {
-        self::assertInstanceOf(NoneInterface::class, $this->none->andThen(static fn ($x) => Some::create($x)));
+        self::assertInstanceOf(NoneInterface::class, $this->none->andThen(static fn (mixed $x): mixed => $x));
     }
 
     /**
@@ -176,7 +173,7 @@ final class NoneTest extends TestCase
      */
     public function testMapOrElse(): void
     {
-        $some = static fn (string $value): string => $value . 'bar';
+        $some = static fn (mixed $value): string => sprintf('%sbar', (string) $value);
 
         $none = static fn (): string => 'baz';
 
@@ -199,6 +196,7 @@ final class NoneTest extends TestCase
 
     /**
      * @covers \Ghostwriter\Option\AbstractOption::__construct
+     * @covers \Ghostwriter\Option\AbstractOption::of
      * @covers \Ghostwriter\Option\AbstractOption::orElse
      * @covers \Ghostwriter\Option\None::__construct
      * @covers \Ghostwriter\Option\None::create
@@ -207,16 +205,11 @@ final class NoneTest extends TestCase
      */
     public function testOrElse(): void
     {
+        $none = None::create();
         $some = Some::create('foo');
+        self::assertSame($none, $this->none);
+        self::assertSame($none, $this->none->orElse(static fn () => $none));
         self::assertSame($some, $this->none->orElse(static fn () => $some));
-        self::assertSame($this->none, $this->none->orElse(static fn () => None::create()));
-
-        $this->expectException(InvalidReturnTypeException::class);
-        self::assertSame($this->none, $this->none->orElse(static function () {
-            /** @var OptionInterface $x */
-            $x = new stdClass();
-            return $x;
-        }));
     }
 
     /**

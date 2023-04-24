7cf2078d0fb85b31f516a8f8e4a00e26f442300b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-24 13:08:33 -0500

    Update Option.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Option.php
+++ src/Option.php
@@ -4,39 +4,24 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Option;
 
-use Ghostwriter\Option\Contract\NoneInterface;
 use Ghostwriter\Option\Contract\OptionInterface;
-use Ghostwriter\Option\Contract\SomeInterface;
 
 /** @see OptionTest */
 final class Option
 {
     /**
-     * @template TValue
+     * @template TOption
      *
-     * @param TValue $value
+     * @param TOption $value
+     *
+     * @return OptionInterface<TOption>
      */
     public static function create(mixed $value): OptionInterface
     {
         return match (true) {
             $value instanceof OptionInterface => $value,
-            $value === null => self::none(),
-            default => self::some($value)
+            $value === null => None::create(),
+            default => Some::create($value)
         };
-    }
-
-    public static function none(): NoneInterface
-    {
-        return None::create();
-    }
-
-    /**
-     * @template TSomeValue
-     *
-     * @param TSomeValue $value
-     */
-    public static function some(mixed $value): SomeInterface
-    {
-        return Some::create($value);
     }
 }

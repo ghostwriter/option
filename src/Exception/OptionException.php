a2f2fe72176d9b36638e20f803ae4f9bd2c8c58b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-25 12:38:17 -0500

    Update OptionException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/OptionException.php
+++ src/Exception/OptionException.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Option\Exception;
 
-use Ghostwriter\Option\Contract\Exception\OptionExceptionInterface;
 use RuntimeException;
 
 final class OptionException extends RuntimeException implements OptionExceptionInterface

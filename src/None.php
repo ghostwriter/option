dd202a18100a91527670c4a1dc54c217b3d1db6e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-15 17:45:43 -0500

    Update None.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/None.php
+++ src/None.php
@@ -4,28 +4,146 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Option;
 
+use Closure;
+use Generator;
+use Ghostwriter\Option\Exception\NullPointerException;
+use Ghostwriter\Option\Exception\OptionException;
+use Ghostwriter\Option\Interface\NoneInterface;
+use Ghostwriter\Option\Interface\OptionInterface;
 use Override;
+use Throwable;
 
 /**
- * @template TNone of never
- *
- * @extends AbstractOption<TNone>
- *
- * @implements NoneInterface<TNone>
- *
  * @see \Tests\Unit\NoneTest
  */
-final class None extends AbstractOption implements NoneInterface
+final class None implements NoneInterface
 {
-    private static null|self $none = null;
+    private static ?self $instance;
 
-    /** @return self<TNone> */
-    #[Override]
-    public static function create(): self
+    private function __construct()
     {
-        /** @var TNone $none */
-        $none = null;
+    }
 
-        return self::$none ??= new self($none);
+    #[Override]
+    public function and(OptionInterface $option): self
+    {
+        return $this;
+    }
+
+    /**
+     * @throws OptionException
+     */
+    #[Override]
+    public function andThen(Closure $function): self
+    {
+        return $this;
+    }
+
+    #[Override]
+    public function contains(mixed $value): bool
+    {
+        return false;
+    }
+
+    /**
+     * @throws Throwable
+     */
+    #[Override]
+    public function expect(Throwable $throwable): mixed
+    {
+        throw $throwable;
+    }
+
+    #[Override]
+    public function filter(Closure $function): self
+    {
+        return $this;
+    }
+
+    #[Override]
+    public function flatten(): self
+    {
+        return $this;
+    }
+
+    #[Override]
+    public function getIterator(): Generator
+    {
+        yield from [];
+    }
+
+    #[Override]
+    public function isNone(): bool
+    {
+        return true;
+    }
+
+    #[Override]
+    public function isSome(): bool
+    {
+        return false;
+    }
+
+    #[Override]
+    public function map(Closure $function): OptionInterface
+    {
+        return $this;
+    }
+
+    #[Override]
+    public function mapOr(Closure $function, mixed $fallback): mixed
+    {
+        return $fallback;
+    }
+
+    #[Override]
+    public function mapOrElse(Closure $function, Closure $fallback): mixed
+    {
+        return $fallback();
+    }
+
+    #[Override]
+    public function or(OptionInterface $option): OptionInterface
+    {
+        return $option;
+    }
+
+    #[Override]
+    public function orElse(Closure $function): OptionInterface
+    {
+        $value = $function();
+
+        return match (true) {
+            $value instanceof OptionInterface => $value,
+            $value === null => self::new(),
+            default => Some::new($value),
+        };
+    }
+
+    /**
+     * @throws NullPointerException
+     */
+    #[Override]
+    public function unwrap(): mixed
+    {
+        throw new NullPointerException();
+    }
+
+    #[Override]
+    public function unwrapOr(mixed $fallback): mixed
+    {
+        return $fallback;
+    }
+
+    #[Override]
+    public function unwrapOrElse(Closure $function): mixed
+    {
+        return $function();
+    }
+
+    #[Override]
+    public static function new(): NoneInterface
+    {
+        return self::$instance ??= new self();
     }
 }

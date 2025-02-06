1b66a50fe4bd8d56acfd63099705245157076d73
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-05 23:01:22 -0600

    Update NoneTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/NoneTest.php
+++ tests/unit/NoneTest.php
@@ -4,76 +4,54 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use Generator;
-use Ghostwriter\Option\Exception\NullPointerException;
 use Ghostwriter\Option\Interface\NoneInterface;
 use Ghostwriter\Option\Interface\OptionInterface;
 use Ghostwriter\Option\Interface\SomeInterface;
 use Ghostwriter\Option\None;
+use Ghostwriter\Option\Option;
 use Ghostwriter\Option\Some;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\Small;
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
 use RuntimeException;
+use Tests\Unit\DataProvider\NoneDataProvider;
 use Throwable;
 
 use function get_debug_type;
-use function iterator_to_array;
 use function sprintf;
 
 #[CoversClass(None::class)]
+#[CoversClass(Option::class)]
 #[CoversClass(Some::class)]
-#[Small]
-final class NoneTest extends TestCase
+final class NoneTest extends AbstractTestCase
 {
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testAnd(): void
     {
-        $some = Some::new('foobar');
-        $none = None::new();
-        $option = $none->and($some);
+        $option = $this->none->and($this->some);
 
-        self::assertSame($none, $option);
+        self::assertSame($this->none, $option);
         self::assertInstanceOf(NoneInterface::class, $option);
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testAndThen(): void
     {
-        $option = None::new()->andThen(
-            /** @return SomeInterface<true> */
-            static fn (): SomeInterface => Some::new(true)
-        );
-
-        self::assertFalse($option->getOr(false));
+        self::assertFalse($this->none->andThen(static fn (): SomeInterface => Some::new(true))->getOr(false));
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testContains(): void
     {
-        $none = None::new();
-        self::assertFalse($none->contains(null));
+        self::assertTrue($this->none->is(null));
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testCreate(): void
     {
-        $none = None::new();
-        self::assertSame($none, None::new());
+        self::assertSame($this->none, None::new());
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testExpect(): void
     {
         $this->expectException(RuntimeException::class);
@@ -82,68 +60,49 @@ final class NoneTest extends TestCase
         None::new()->expect(new RuntimeException(__FUNCTION__));
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testFilter(): void
     {
-        self::assertInstanceOf(NoneInterface::class, None::new()->filter(static fn (mixed $x): bool => null === $x));
+        self::assertInstanceOf(
+            NoneInterface::class,
+            $this->none->filter(static fn (mixed $x): bool => null === $x),
+        );
     }
 
-    /**
-     * @throws Throwable
-     */
-    public function testFlatten(): void
+    /** @throws Throwable */
+    public function testGetOr(): void
     {
-        $none = None::new();
-        $option = $none->flatten();
-        self::assertSame($none, $option);
-        self::assertInstanceOf(NoneInterface::class, $option);
+        self::assertSame('GetOr', $this->none->getOr('GetOr'));
     }
 
-    /**
-     * @throws Throwable
-     */
-    public function testGetIterator(): void
+    /** @throws Throwable */
+    public function testGetOrElse(): void
     {
-        $none = None::new();
-        self::assertCount(0, iterator_to_array($none));
+        self::assertSame('GetOrElse', $this->none->getOrElse(static fn (): string => 'GetOrElse'));
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testIsNone(): void
     {
-        $none = None::new();
-        self::assertTrue($none->isNone());
+        self::assertTrue($this->none->isNone());
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testIsSome(): void
     {
-        $none = None::new();
-        self::assertFalse($none->isSome());
+        self::assertFalse($this->none->isSome());
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testMap(): void
     {
-        $none = None::new();
-        self::assertInstanceOf(NoneInterface::class, $none->map(/** @return 0 */ static fn (): int => 0));
+        self::assertInstanceOf(NoneInterface::class, $this->none->map(static fn (): int => 0));
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testMapOr(): void
     {
-        $none = None::new();
-        self::assertSame('baz', $none->mapOr(/** @return 0 */ static fn (): int => 0, 'baz'));
+        self::assertSame('baz', $this->none->mapOr(static fn (): int => 0, 'baz'));
     }
 
     /**
@@ -153,20 +112,14 @@ final class NoneTest extends TestCase
      */
     public function testMapOrElse(): void
     {
-        $none = None::new();
+        $someFn = static fn (mixed $value): string => sprintf('%sbar', get_debug_type($value));
 
-        /**
-         * @var Closure(TMixed):non-empty-string $someFn
-         */
-        $someFn = static fn (mixed $value): mixed => sprintf('%sbar', get_debug_type($value));
+        $noneFn = static fn (): string => 'baz';
 
-        $noneFn = /** @return 'baz' */ static fn (): string => 'baz';
-
-        self::assertSame('baz', $none->mapOrElse($someFn, $noneFn));
+        self::assertSame('baz', $this->none->mapOrElse($someFn, $noneFn));
     }
 
     /**
-     *
      * @template TValue
      *
      * @param class-string $expected
@@ -174,85 +127,34 @@ final class NoneTest extends TestCase
      *
      * @throws Throwable
      */
-    #[DataProvider('ofDataProvider')]
-    public function testOptionCreate(string $expected, mixed $value): void
+    #[DataProviderExternal(NoneDataProvider::class, 'provide')]
+    public function testNew(string $expected, mixed $value): void
     {
-        $option = Some::nullable($value);
+        $none = None::new();
+
+        self::assertInstanceOf($expected, $none);
 
         if ($value instanceof OptionInterface) {
-            self::assertSame($value, $option);
+            self::assertSame($value, $none);
         }
 
-        self::assertInstanceOf($expected, $option);
+        self::assertInstanceOf(NoneInterface::class, $none);
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testOr(): void
     {
-        $none = None::new();
-        $some = Some::new('foobar');
-        self::assertSame($some, $none->or($some));
+        self::assertSame($this->some, $this->none->or($this->some));
     }
 
-    /**
-     * @throws Throwable
-     */
+    /** @throws Throwable */
     public function testOrElse(): void
     {
-        $none = None::new();
+        $none = $this->none;
+        $some = $this->some;
 
-        $some = Some::new('foo');
+        self::assertSame($none, $this->none->orElse(static fn (): NoneInterface => $none));
 
-        self::assertSame($none, $none->orElse(static fn (): NoneInterface => $none));
-
-        self::assertSame($some, $none->orElse(
-            /**
-             * @return SomeInterface<'foo'>
-             */
-            static fn (): SomeInterface => $some
-        ));
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testUnwrap(): void
-    {
-        $none = None::new();
-        $this->expectException(NullPointerException::class);
-        $none->get();
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testUnwrapOr(): void
-    {
-        $none = None::new();
-
-        self::assertSame('UnwrapOr', $none->getOr('UnwrapOr'));
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testUnwrapOrElse(): void
-    {
-        $none = None::new();
-
-        $function = /** @return 'UnwrapOrElse'  */ static fn (): string => 'UnwrapOrElse';
-
-        self::assertSame('UnwrapOrElse', $none->getOrElse($function));
-    }
-
-    /**
-     * @return Generator<'None::class'|'null',array{0:class-string<NoneInterface>,1:null|NoneInterface}>
-     */
-    public static function ofDataProvider(): Generator
-    {
-        yield 'null' => [NoneInterface::class, null];
-        yield 'None::class' => [NoneInterface::class, None::new()];
+        self::assertSame($some, $this->none->orElse(static fn (): SomeInterface => $some));
     }
 }

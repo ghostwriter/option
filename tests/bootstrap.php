ab1878c83fe6013c762fab4df3d3dfdb38380330
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-05 23:01:21 -0600

    Add bootstrap.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Composer\Autoload\ClassLoader;
+
+/** @var ClassLoader $classLoader */
+$classLoader = require \dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+
+if (! $classLoader instanceof ClassLoader) {
+    /** @psalm-suppress UncaughtThrowInGlobalScope */
+    throw new \RuntimeException('Class loader not found');
+}
+
+$fixturePath = __DIR__ . \DIRECTORY_SEPARATOR . 'fixture';
+
+/** @psalm-suppress UncaughtThrowInGlobalScope */
+$classLoader->addPsr4('Tests\\Fixture\\', $fixturePath);
+
+return $classLoader;
